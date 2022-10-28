9f4104a670b0323d2088fd914ed479137dfb02eb
Vincent <vincentwarmerdam@gmail.com>
2022-10-28 13:03:08 +0200

    ben er ekkes klaar mee

--- docs/examples/border-base.py
+++ docs/examples/border-base.py
@@ -7,9 +7,9 @@ class BorderDemo(App):
 
     def compose(self) -> ComposeResult:
         """Called to add widgets to the app."""
-        yield Static("first",   classes="p-2 text-center bg-gray-100 border-solid-gray-900")
-        yield Static("second",  classes="p-2 text-center bg-gray-200 border-round-gray-900")
-        yield Static("third",  classes="p-2 text-center bg-gray-400 border-double-gray-900")
+        yield Static("first",  classes="p-2 text-center text-gray-800 bg-gray-100 border-solid-gray-900")
+        yield Static("second", classes="p-2 text-center text-gray-800 bg-gray-200 border-round-gray-900")
+        yield Static("third",  classes="p-2 text-center text-gray-800 bg-gray-400 border-double-gray-900")
 
 if __name__ == "__main__":
     app = BorderDemo()
--- docs/examples/border-sides.py
+++ docs/examples/border-sides.py
@@ -7,12 +7,12 @@ class BorderSidesDemo(App):
 
     def compose(self) -> ComposeResult:
         """Called to add widgets to the app."""
-        yield Static("t", classes="p-1 text-center bg-gray-200 border-t-heavy-gray-600")
-        yield Static("l", classes="p-1 text-center bg-gray-300 border-l-heavy-gray-600")
-        yield Static("r", classes="p-1 text-center bg-gray-200 border-r-heavy-gray-600")
-        yield Static("b", classes="p-1 text-center bg-gray-300 border-b-heavy-gray-600")
-        yield Static("x", classes="p-1 text-center bg-gray-200 border-x-heavy-gray-600")
-        yield Static("y", classes="p-1 text-center bg-gray-300 border-y-heavy-gray-600")
+        yield Static("t", classes="p-1 text-center text-gray-800 bg-gray-200 border-t-heavy-gray-600")
+        yield Static("l", classes="p-1 text-center text-gray-800 bg-gray-300 border-l-heavy-gray-600")
+        yield Static("r", classes="p-1 text-center text-gray-800 bg-gray-200 border-r-heavy-gray-600")
+        yield Static("b", classes="p-1 text-center text-gray-800 bg-gray-300 border-b-heavy-gray-600")
+        yield Static("x", classes="p-1 text-center text-gray-800 bg-gray-200 border-x-heavy-gray-600")
+        yield Static("y", classes="p-1 text-center text-gray-800 bg-gray-300 border-y-heavy-gray-600")
 
 if __name__ == "__main__":
     app = BorderSidesDemo()
